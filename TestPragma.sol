c7f6298f6a68da2192a0efcf848233ddacfe9829
sam bacha <sam@freighttrust.com>
2021-08-03 22:40:15 -0700

    Create TestPragma.sol

---
+++ TestPragma.sol
@@ -0,0 +1,14 @@
+// SPDX-LicenseIdentifier: MIT
+
+pragma solidity >=0.4.21   <0.6.0;
+pragma solidity >=0.4.21 <=0.6.0    ;
+pragma solidity    >0.4.21 <0.6.0;
+pragma solidity >0.4.21 <=0.6.0;
+pragma solidity >=0.4.21<0.6.0;
+pragma solidity   ^ 0.4.21  ;
+pragma solidity  ~ 0.4.21  ;
+
+
+// Solidity versioning says this is part of the specification
+// @test-ignore
+pragma solidity 0.5.0 || 0.6.0 || 0.7.0;
